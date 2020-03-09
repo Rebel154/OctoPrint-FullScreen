548b2cd50c1d6f488617161729a48edc28fa2dd6
Rebel154 <reb@reb.me.uk>
2020-03-09 18:22:08 +0000

    Updated details to me

--- setup.py
+++ setup.py
@@ -14,20 +14,20 @@ plugin_package = "octoprint_fullscreen"
 plugin_name = "OctoPrint-Fullscreen"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.4"
+plugin_version = "0.0.5"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = """Open webcam in fullscreen mode"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Paul de Vries"
+plugin_author = "Paul de Vries & Roy Brown"
 
 # The plugin's author's mail address.
-plugin_author_email = "octoprint+fullscreen@aerosol.me"
+plugin_author_email = "rebel154@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/BillyBlaze/OctoPrint-FullScreen"
+plugin_url = "https://github.com/Rebel154/OctoPrint-FullScreen"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
